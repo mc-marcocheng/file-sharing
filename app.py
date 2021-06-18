abaa87b5ba1840863c07ff1628a9c91b8c7ae8cf
mc-marcocheng <mccheng990503@gmail.com>
2021-06-18 19:05:55 +0800

    4.7.6

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = dash.Dash(__name__, server=server)
 
 app.layout = html.Div([
     html.H3(id='cur'),
-    dcc.Slider(id='slider', value=0, min=-10, max=10, step=0.1),
+    dcc.Slider(id='slider', value=4, min=0, max=8, step=0.1),
     html.H3(id='iter'),
     dcc.Slider(id='slider2', value=1, min=1, max=10, step=1),
     dcc.Graph(id = 'live-graph', animate = True),
@@ -30,22 +30,24 @@ app.layout = html.Div([
 def make_fig(cur_x, iter):
     colors = ['#636EFA', '#EF553B', '#00CC96', '#AB63FA', '#FFA15A', '#19D3F3', '#FF6692', '#B6E880', '#FF97FF', '#FECB52']
     saved = cur_x
+    f = lambda x: np.sign(x-4)*np.sqrt(np.sign(x-4)*(x-4))
+    df = lambda x: 1/(2*np.sqrt((x-4)*np.sign(x-4)))
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=(xx := np.linspace(-10,10,1000)), y=np.abs(xx)**(1/3)*np.sign(xx), mode='lines', line=dict(color='Green'), name='f(x)=x^(1/3)'))
+    fig.add_trace(go.Scatter(x=(xx := np.linspace(0,8,500)), y=f(xx), mode='lines', line=dict(color='Green'), name="f(x)"))
     for i in range(iter):
-        if saved:
-            fig.add_trace(go.Scatter(x=[saved]*2, y=[0, np.abs(saved)**(1/3)*np.sign(saved)], mode='lines', line=dict(color=colors[i], dash='dash'), showlegend=False))
-        fig.add_trace(go.Scatter(x=[saved], y=[np.abs(saved)**(1/3)*np.sign(saved)], mode='markers', line=dict(color=colors[i]), showlegend=False,
+        if saved != 4:
+            fig.add_trace(go.Scatter(x=[saved]*2, y=[0, f(saved)], mode='lines', line=dict(color=colors[i], dash='dash'), showlegend=False))
+        fig.add_trace(go.Scatter(x=[saved], y=[f(saved)], mode='markers', line=dict(color=colors[i]), showlegend=False,
                                 marker=dict(
                                     color=colors[i],
                                     size=5,
                                     line=dict(color=colors[i], width=2)
                                 )))
-        if saved==0:
-            fig.add_trace(go.Scatter(x=[0,0], y=[-100,100], mode='lines', line=dict(color=colors[i]), name="Iteration "+str(i+1)))
+        if saved == 4:
+            fig.add_trace(go.Scatter(x=[4,4], y=[-100,100], mode='lines', line=dict(color=colors[i]), name="Iteration "+str(i+1)))
         else:
-            fig.add_trace(go.Scatter(x=xx, y=(saved*np.sign(saved)/(3*np.abs(saved)**(5/3)))*(xx-saved)+np.abs(saved)**(1/3)*np.sign(saved), mode='lines', line=dict(color=colors[i]), name="Iteration "+str(i+1)))
-        saved = 0 if saved==0 else (np.abs(saved)**(1/3)*np.sign(saved))/(-saved*np.sign(saved)/(3*np.abs(saved)**(5/3)))+saved
+            fig.add_trace(go.Scatter(x=xx, y=(df(saved))*(xx-saved)+f(saved), mode='lines', line=dict(color=colors[i]), name="Iteration "+str(i+1)))
+        saved = 4 if saved == 4 else f(saved)/(-df(saved))+saved
         fig.add_trace(go.Scatter(x=[saved], y=[0], mode='markers', line=dict(color=colors[i]), showlegend=False,
                                 marker=dict(
                                     color=colors[i],
@@ -67,10 +69,10 @@ def make_fig(cur_x, iter):
             title_standoff=10),
             )
     fig.update(layout_showlegend=True)
-    fig.update_yaxes(range=[-2.5, 2.5])
-    fig.update_xaxes(range=[-10, 10])
+    fig.update_yaxes(range=[-2, 2])
+    fig.update_xaxes(range=[0, 8])
 
-    return fig, f'x{iter+1}={saved:.9g}', f'x1={cur_x}', f'Number of iterations = {iter}'
+    return fig, f'Estimated root: {saved:.2f}', f'x1={cur_x}', f'Number of iterations = {iter}'
 
 if __name__ == '__main__':
     app.server.run(debug=True, threaded=True)
